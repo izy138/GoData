684dace3aae8ff1b10389d7127762820b1b05eda
izy138 <Isabellapc97@gmail.com>
2025-08-19 14:40:38 -0700

    pageOffset, loadPage

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"os" 				// for file opterations like create,open,read,write
 )
 
-//database rules
+// database rules
 const (
 	PageSize = 4096 // db stores data in chunks calls pages. 4KB is the common size 
 	HeaderSize = 64 // the first 64 bytes of a file will contain metadata about my db
@@ -17,7 +17,7 @@ const (
 )
 
 // data container - Pages hold the data, and the db needs to know what page its looking at, 
-//  whats inside it and whether changes have been made.
+// whats inside it and whether changes have been made.
 type Page struct {
 	ID 			uint32 				// tells us which page it is (Page1,2,etc)
 	Data 		[PageSize]byte		// the 4KD of storage for the key-value pairs
@@ -35,7 +35,7 @@ type Storage struct {
 	totalPages  uint32			// how many pages exist in total
 }
 
-//when opening a db file, we need to know how its organized, its a header tag that acts like a table of contents
+// when opening a db file, we need to know how its organized, its a header tag that acts like a table of contents
 type Header struct {
 	Magic 		uint32 		// 'MYDB' signature to verify the file 
 	Version 	uint32		// the version of our databases format
@@ -62,7 +62,7 @@ func NewStorage(filename string) (*Storage,error) {
 	}
 
 	// creates the Storage struct and initialize the pageIndex and pages mappings,
-	//  which both start as empty. sets the file we opened/created to the storage.
+	// which both start as empty. sets the file we opened/created to the storage.
 	storage := &Storage {
 		file: 		file,
 		pageSize: 	PageSize,
@@ -76,7 +76,7 @@ func NewStorage(filename string) (*Storage,error) {
 		return nil, fmt.Errorf("failed to stat file: %w", err)
 	}
 
-	//if the size is 0 then that it is an empty file, so we set up a new db
+	// if the size is 0 then that it is an empty file, so we set up a new db
 	// stat.Size checks how many bytes are in the file
 	if stat.Size() == 0 {
 		// initializes a new file, with header
@@ -112,7 +112,7 @@ func NewStorage(filename string) (*Storage,error) {
 
 // we a have new empty file, that we want to become a database.
 func (s *Storage) initializeNewFile() error {
-	//we create the header struct for it.
+	// we create the header struct for it.
 	// the "birth certificate" literally the header of any notebook page: name, date,"page count: 0"
 	header := Header {
 		Magic:		MagicNumber,		// sig that identifies the db file
@@ -122,12 +122,12 @@ func (s *Storage) initializeNewFile() error {
 		nextPageID: 0,					// WHen we create the first page, it will start as page 0)
 	}
 
-	//updates the in-memory Storage object to match the header.
+	// updates the in-memory Storage object to match the header.
 	// tracks the state of the db
 	s.nextPageID = 0;
 	s.totalPages = 0;
 	
-	//calls another function to actually write the 64 bytes to the file.
+	// calls another function to actually write the 64 bytes to the file.
 	return s.writeHeader(&header) //passes a pointer address to the header 
 // NEW DATABASE INITIALIZATION:
 // 1. We have an empty file (0 bytes)
@@ -161,8 +161,8 @@ func (s *Storage) initializeNewFile() error {
 func (s *Storage) writeHeader(header *Header) error {
 	headerBytes := make([]byte, HeaderSize) // makes an empty 64 byte array
 	
-	//coverts the numbers into bytes to be stored into the 64-byte array headerBytes
-	//PutUInt32 puts the unisigned int 32 bit 
+	// coverts the numbers into bytes to be stored into the 64-byte array headerBytes
+	// PutUInt32 puts the unisigned int 32 bit 
 	binary.LittleEndian.PutUint32(headerBytes[0:4], header.Magic)
 	binary.LittleEndian.PutUint32(headerBytes[4:8], header.Version)
 	binary.LittleEndian.PutUint32(headerBytes[8:12], header.PageSize)
@@ -175,7 +175,7 @@ func (s *Storage) writeHeader(header *Header) error {
 	if err != nil {
 		return fmt.Errorf("failed to read header: %w", err)
 	}
-	//forces the OS to wrtie the data to the disk
+	// forces the OS to wrtie the data to the disk
 	// without doing this, the data could sit in memory and be lost with program crash
 	return s.file.Sync()
 // 	CREATING A NEW DATABASE:
@@ -199,17 +199,17 @@ func (s *Storage) writeHeader(header *Header) error {
 // we loaded a file that contains data
 // this will read the header to understand how its organized
 func (s *Storage) loadHeader() error{
-	//create empty 64 byte aray for reading the header
+	// create empty 64 byte aray for reading the header
 	headerBytes := make([]byte,HeaderSize)
 
-	//opens and reads the file header from the start
+	// opens and reads the file header from the start
 	_,err := s.file.ReadAt(headerBytes,0)
 	if err != nil {
 		return fmt.Errorf("failed to read header: %w", err)
 	}
 
-	//converts the BYTES back into numbers
-	//Uint32 converts 4 bytes back into a 32 bit number
+	// converts the BYTES back into numbers
+	// Uint32 converts 4 bytes back into a 32 bit number
 	header := Header {
 		Magic: binary.LittleEndian.Uint31(headerBytes[0:4])
 		Version: binary.LittleEndian.Uint31(headerBytes[4:8])
@@ -218,7 +218,7 @@ func (s *Storage) loadHeader() error{
 		NextPageID: binary.LittleEndian.Uint31(headerBytes[16:20])
 	}
 
-	//validates the header info
+	// validates the header info
 	if header.Magic != MagicNumber {
 		return errors.New("invalid file format: magic number mismatch")
 	}
@@ -229,8 +229,8 @@ func (s *Storage) loadHeader() error{
 		return fmt.Errorf("page size mismatch: expected %d, got %d", s.pageSize, header.PageSize)
 	}
 
-	//updates the Storage object
-	//sets the variables to match the file
+	// updates the Storage object
+	// sets the variables to match the file
 	s.nextPageID = header.NextPageID
 	s.totalPages = header.TotalPages
 	
@@ -259,13 +259,13 @@ func (s *Storage) loadHeader() error{
 // 7. Ready to work with existing database!
 } 
 
-//we opened an existing database, there are pages with data,
-//but dont know what kets are stored and where
+// we opened an existing database, there are pages with data,
+// but dont know what kets are stored and where
 func(s *Storage) buildIndex() error {
-	//loops through all the pages. s.totalPages = 3 it loops though pageID 0,1,2
+	// loops through all the pages. s.totalPages = 3 it loops though pageID 0,1,2
 	for pageID := uint32(0); pageID < s.totalPages; pageID++ {
 
-		//loads each page into memory
+		// loads each page into memory
 		page, err := s.loadPage(pageID)
 		if err != nil {
 			return fmt.Errorf("failed to load page %d during index build: %w", pageID,err)
@@ -297,18 +297,18 @@ func(s *Storage) buildIndex() error {
 			// move the position forward by 4 bytes to get to the value indexes
 			offset += 4
 			
-			//makes sure we dont read past the end of the page. 
+			// makes sure we dont read past the end of the page. 
 			if offset+int(keyLen)+int(valueLen) > len(page.Data){
 				break
 			}
 
-			//key is recorded using the current offset and the key length.
-			//converts the bytes into a string (key)
+			// key is recorded using the current offset and the key length.
+			// converts the bytes into a string (key)
 			key := string(page.Data[offset:offset+int(keyLen)])
-			//adds to key to index: "key _ is stored in page 0"
+			// adds to key to index: "key _ is stored in page 0"
 			s.pageIndex[key] = pageID
 
-			//the offset moves up past the key and value, 
+			// the offset moves up past the key and value, 
 			// to record the next key and value length and continue the loop until the page ends.
 			offset += int(keyLen) + int(valueLen)
 		}	
@@ -336,6 +336,79 @@ func(s *Storage) buildIndex() error {
 // 7. Move to next page
 //    ↓
 // 8. When done: pageIndex contains location of every key!
-
 }
+
+// pageOffset() - Calculate where pages live in the file
+// loadPage() - Read a page from disk into memory
+// writePage() - Write a page from memory to disk
+// allocateNewPage() - Create a brand new page
+// updateHeader() - Save current database state
+
+//calculates the exact address where the page is stored in the file
+func (s *Storage) pageOffset(pageID uint32) int64 {
+	return int64(HeaderSize + pageID*uint32(s.pageSize))
+}
+//0-63 : the header
+//64-4159 : Page 0
+//4160-8255 : Page 1
+//8256-12351 : Page 2
+
+// Example:
+// pageID = 0
+// HeaderSize = 64 bytes
+// s.pageSize = 4096 bytes
+// offset = 64 + (0 * 4096) = 64
+
+// pageID = 2
+// offset = 64 + (2 * 4096) = 64 + 8192 = 8256
+
+func (s *Storage) loadPage(pageID uint32) (*Page,error) {
+	// checks if the page is in cache already
+	// looks in the in-memory cache (the s.pages map)
+	// **reading directly from memory is 1000x faster than reading from the disk
+	if page, exists := s.pages[pageID]; exists{
+		return page,nil
+	}
+
+	// reads the page from disk
+	offset := s.pageOffset(pageID) // uses the pageOffset() function to find the exact byte position
+	pageData := make([]byte, s.pageSize) // creates a 4096 byte array to hold the page data to hold the data read from disk
+
+	_, err := s.file.ReadAt(pageData, offset) // reads exactly 4096 bytes starting at the calculated offset
+	// ReadAt lets you read from any position in the file
+	// example: we want Page 1 which starts from 4160-8255.
+	// so it will be: s.file.ReadAt(pageData, 4160)
+
+
+	// creates a page object
+	page := &Page{
+		ID: 	 pageID,
+		isDirty: false,
+	}
+	copy(page.Data[:], pageData)
+	// creates a new page struct and sets the ID and marks it as clean (isDirty = false because it has not been changed ie it matches whats on the disk)
+
+	// next we parse the page metadata, every page has a mini header
+	if len(pageData) >= 2 {
+		page.RecordCount = binary.LittleEndian.Uint16(pageData[0:2])
+	}
+	// bytes 0-1: Record count (how many key-value pairs are in this page)
+	// bytes 2+: Actual records (key-value pairs)
+
+	// so example lets say there are 3 records (3 key value pairs) in a page
+	// uint16 is 16 bits, so 2 bytes.
+	// we want to store the number 3 in these 16 bits.
+	// Decimal = 3 , Binary: 00000011, Hex: 0x03
+	// we need 16 bits so in binary: 00000000 00000011 and hex: 0x00 0x03
+	// Big Endian is the most significant bit first: 0x00, 0x03
+	// Little Endian is the least significant bit first: 0x03, 0x00
+	// so when we get the pageData it would be: binary.LittleEndian.Uint16([0x03, 0x00]) = 3
+
+	// Cache the loaded page
+	// stores the page in memory cache for faster future access
+	s.pages[pageID] = page
+
+	return page, nil
+}
+//the first access in Disk would be ~5ms, the second acces in memeory would be ~0.0005ms (1000x faster)
 

466807e25ff319da0e8b448ed8f3fabe44d7c625
izy138 <Isabellapc97@gmail.com>
2026-01-07 20:11:59 -0500

    wal functions

--- go.mod
+++ go.mod
@@ -2,3 +2,4 @@ module godata
 
 go 1.21
 
+
--- wal.go
+++ wal.go
@@ -42,3 +42,136 @@ func (e *LogEntry) Serialize() []byte {
 	data := make([]byte, totalSize)
 	
 	offset := 0
+
+	// Write entry info to the byte array
+	binary.LittleEndian.PutUint64(data[offset:offset+8], e.LSN)
+	offset += 8
+	binary.LittleEndian.PutUint32(data[offset:offset+4], e.EntrySize)
+	offset += 4
+	binary.LittleEndian.PutUint8(data[offset:offset+1], e.Type)
+	offset += 1
+	binary.LittleEndian.PutUint16(data[offset:offset+2], e.KeyLen)
+	offset += 2
+	binary.LittleEndian.PutUint16(data[offset:offset+2], e.ValueLen)
+	offset += 2
+
+	copy(data[offset:offset+len(e.Key)], []byte(e.Key))
+	offset += len(e.Key)
+	copy(data[offset:offset+len(e.Value)], []byte(e.Value))
+	offset += len(e.Value)
+
+
+	// data = [
+    // // LSN (8 bytes)
+    // 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    // // EntrySize (4 bytes) 
+    // 0x21, 0x00, 0x00, 0x00, 
+    // // Type (1 byte)
+    // 0x01,  // PUT
+    // // KeyLen (2 bytes)
+    // 0x06, 0x00,  
+    // // ValueLen (2 bytes)
+    // 0x04, 0x00, 
+    // // Key "user:1" (6 bytes)
+    // 0x75, 0x73, 0x65, 0x72, 0x3A, 0x31,  // u s e r : 1
+    // // Value "john" (4 bytes)
+    // 0x6A, 0x6F, 0x68, 0x6E,  // j o h n
+    // // Checksum space (4 bytes) - still empty!
+    // 0x00, 0x00, 0x00, 0x00
+	// ]
+	// offset = 27 (bytes 0-26) 
+
+	//checksum is a fingerprint for the data. It is a single number that represents all the data.
+	//it is used to detect corruption of the data. it is calculated by taking the data and running it through a hash function. returns a single number. if one byte changes, the checksum will change, alerting you that something is wrong.
+
+	// checksumData = data[0:26] 
+	//bytes 0-26 contain all the entry info and the key and value.
+	checksumData := data[0:offset] //we dont use data[0:] because we dont want to include the checksum in the checksum calculation. 
+
+	//this runs the CRC32 hash function on the checksumData and returns a 32 bit number.
+	//very sensitive to small changes in the data.
+	//Input:  27 bytes [0x01, 0x00, 0x00, ..., 0x6E] 
+	//Output: 0x8A3F2B1C (a single 32-bit number)
+	checksum := crc32.ChecksumIEEE(checksumData)
+
+	//this converts the checksum into 4 bytes and writes it to the data array at the offset.
+	binary.LittleEndian.PutUint32(data[offset:offset+4], e.Checksum)
+
+	//Before:
+	//data[27:31] = [0x00, 0x00, 0x00, 0x00]  // Empty checksum space
+
+	//After PutUint32 with checksum = 0x8A3F2B1C:
+	//data[27:31] = [0x1C, 0x2B, 0x3F, 0x8A]  // Little-endian bytes
+
+	return data
+}
+
+// Deserialize converts a byte slice into a LogEntry object
+func Deserialize(data []byte) (*LogEntry, error) {
+	//need at least minimum header size initialized
+	minHeaderSize := 8 + 4 + 1 + 2 + 2 + 4 // LSN, EntrySize, Type, KeyLen, ValueLen, Checksum
+	if len(data) < minHeaderSize {
+		return nil, errors.New("insufficient data for log entry header")
+	}
+
+	offset := 0
+	entry := &LogEntry{}
+
+	// Read LSN (8 bytes)
+	entry.LSN = binary.LittleEndian.Uint64(data[offset:offset+8])
+	offset += 8
+	// Read EntrySize (4 bytes)
+	entry.EntrySize = binary.LittleEndian.Uint32(data[offset:offset+4])
+	offset += 4
+
+	// Validate we have enough data
+	if len(data) < int(entry.EntrySize) {
+		return nil, errors.New("incomplete log entry")
+	}
+
+	// Read Type (1 byte)
+	entry.Type = data[offset]
+	offset += 1
+	// Read KeyLen (2 bytes)
+	entry.KeyLen = binary.LittleEndian.Uint16(data[offset:offset+2])
+	offset += 2
+	// Read ValueLen (2 bytes)
+	entry.ValueLen = binary.LittleEndian.Uint16(data[offset:offset+2])
+	offset += 2
+
+	// Read Key
+	if offset+int(entry.KeyLen) > len(data) {
+		return nil, errors.New("invalid key length")
+	}
+	entry.Key = string(data[offset : offset+int(entry.KeyLen)])
+	offset += int(entry.KeyLen)
+	
+	// Read Value
+	if offset+int(entry.ValueLen) > len(data) {
+		return nil, errors.New("invalid value length")
+	}
+	entry.Value = string(data[offset : offset+int(entry.ValueLen)])
+	offset += int(entry.ValueLen)
+	
+	// Read Checksum (4 bytes)
+	if offset+4 > len(data) {
+		return nil, errors.New("missing checksum")
+	}
+	entry.Checksum = binary.LittleEndian.Uint32(data[offset : offset+4])
+	
+	return entry, nil
+}
+
+func (e *LogEntry) ValidateChecksum() bool {
+
+	//re-serialize the entry
+	data := e.Serialize()
+
+	//calculate the checksum of data except the last 4 bytes
+	checksumData := data[0:len(data)-4]
+	//run the CRC32 hash function on the checksumData and returns a 32 bit number.
+	calculatedChecksum := crc32.ChecksumIEEE(checksumData)
+
+	//compare the checksum of the re-serialized data to the checksum in the entry
+	return calculatedChecksum == e.Checksum
+}
